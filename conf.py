db05df54457af362dec5c9f70f7e09205499a77c
Alessandro <alessandro.portosa@eng.it>
2018-05-21 16:44:55 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -31,7 +31,7 @@ if on_rtd:
         'css_files': [
             'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            'https://raw.githubusercontent.com/KnowageLabs/Knowage-Documentation/master/style/knowage_readthedocs.css',
+            #'https://raw.githubusercontent.com/KnowageLabs/Knowage-Documentation/master/style/knowage_readthedocs.css',
         ],
     }
 else:
