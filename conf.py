57872c557e0842892a6757f9bec519fd13290cd7
NunziaM <39482777+NunziaM@users.noreply.github.com>
2018-06-06 17:31:03 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -59,7 +59,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig'
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.numfig'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
