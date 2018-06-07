d28cd12a701fc85f92b4f2faa978ad93493c84c6
NunziaM <39482777+NunziaM@users.noreply.github.com>
2018-06-07 10:35:41 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ else:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('./extensions'))
+# sys.path.insert(0, os.path.abspath('./extensions'))
 
 # -- General configuration ------------------------------------------------
 
@@ -59,8 +59,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig',
-    'numfig'
+    'sphinx.ext.ifconfig'# ,
+#    'numfig'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
